22f0f2d598df5dbcf0b87b0904ef8ff080c4619e
hyhwang <hyihua652@gmail.com>
2019-03-14 15:05:26 +0800

    1

--- app/src/main/java/cs2c/tku/myapplication/MainActivity.java
+++ app/src/main/java/cs2c/tku/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package cs2c.tku.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int tsize=24;
+    public void bigger(View view){
+        TextView h;
+        h=findViewById(R.id.textView);
+        h.setTextSize(++tsize);
     }
 }
